c4c2c277049a37800b3da1ca5415d1ee1df65684
SANTOSH SAHOO <101939016+Santosh-2003-sahoo@users.noreply.github.com>
2024-07-23 02:42:50 +0530

    added degree change funct

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ let weather = {
         .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
+
+      
+
       const { name } = data.city ;
       const { icon, description } = data.list[0].weather[0];
       const { temp,feels_like, humidity,temp_min,temp_max } = data.list[0].main;
@@ -28,52 +31,67 @@ let weather = {
         "https://openweathermap.org/img/wn/" + icon + ".png";
       document.querySelector(".description").innerText = description;
       document.querySelector(".dt").innerText = "last updated:  "+ datetime;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".temp-feels-like").innerText ="Feels like: "+feels_like + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind speed: " + speed + " km/h";
-        document.querySelector(".temp-avg").innerText = "Avg Temp : "+(temp_min+temp_max)/2 + "°C";
-      document.querySelector(".temp-min").innerText = "Min Temp : "+temp_min + "°C";
-      document.querySelector(".temp-max").innerText = "Max Temp : "+temp_max + "°C";
-      document.querySelector(".weather").classList.remove("loading");
-      // document.body.style.backgroundImage = url(`https://www.pexels.com/search/mumbai/`);
 
-
-      //weather forecast
-      
-      document.querySelector(".icon1").src =
-        "https://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + ".png";
-      document.querySelector(".temp1").innerText = data.list[1].main.temp + "°C";
-      document.querySelector(".dt1").innerText = data.list[3].dt_txt;
-
-      document.querySelector(".icon2").src =
+      const convertTemp = (temp, unit) => {
+        if (unit === "F") {
+          return ((temp * 9 / 5) + 32).toFixed(2);
+      } else if (unit === "K") {
+          return (temp + 273.15).toFixed(2);
+      } else {
+          return temp.toFixed(2);
+      }
+    };
+    
+    const selectedUnit = ttt;
+    
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".dt").innerText = "Last updated: " + datetime;
+    
+    document.querySelector(".temp").innerText = convertTemp(temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".temp-feels-like").innerText = "Feels like: " + convertTemp(feels_like, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".temp-avg").innerText = "Avg Temp: " + convertTemp((temp_min + temp_max) / 2, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".temp-min").innerText = "Min Temp: " + convertTemp(temp_min, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".temp-max").innerText = "Max Temp: " + convertTemp(temp_max, selectedUnit) + " °" + selectedUnit;
+    
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    
+    document.querySelector(".weather").classList.remove("loading");
+    
+    document.querySelector(".icon1").src =
         "https://openweathermap.org/img/wn/" + data.list[6].weather[0].icon + ".png";
-      document.querySelector(".temp2").innerText = data.list[6].main.temp + "°C";
-      document.querySelector(".dt2").innerText = data.list[6].dt_txt;
-
-      document.querySelector(".icon3").src =
-        "https://openweathermap.org/img/wn/" + data.list[9].weather[0].icon + ".png";
-      document.querySelector(".temp3").innerText = data.list[9].main.temp + "°C";
-      document.querySelector(".dt3").innerText = data.list[9].dt_txt;
-
-      document.querySelector(".icon4").src =
+    document.querySelector(".temp1").innerText = convertTemp(data.list[6].main.temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".dt1").innerText = data.list[6].dt_txt;
+    
+    document.querySelector(".icon2").src =
         "https://openweathermap.org/img/wn/" + data.list[12].weather[0].icon + ".png";
-      document.querySelector(".temp4").innerText = data.list[12].main.temp + "°C";
-      document.querySelector(".dt4").innerText = data.list[12].dt_txt;
+    document.querySelector(".temp2").innerText = convertTemp(data.list[12].main.temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".dt2").innerText = data.list[12].dt_txt;
+    
+    document.querySelector(".icon3").src =
+        "https://openweathermap.org/img/wn/" + data.list[18].weather[0].icon + ".png";
+    document.querySelector(".temp3").innerText = convertTemp(data.list[18].main.temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".dt3").innerText = data.list[18].dt_txt;
+    
+    document.querySelector(".icon4").src =
+        "https://openweathermap.org/img/wn/" + data.list[24].weather[0].icon + ".png";
+    document.querySelector(".temp4").innerText = convertTemp(data.list[24].main.temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".dt4").innerText = data.list[24].dt_txt;
+    
+    document.querySelector(".icon5").src =
+        "https://openweathermap.org/img/wn/" + data.list[30].weather[0].icon + ".png";
+    document.querySelector(".temp5").innerText = convertTemp(data.list[30].main.temp, selectedUnit) + " °" + selectedUnit;
+    document.querySelector(".dt5").innerText = data.list[30].dt_txt;
 
-      document.querySelector(".icon5").src =
-        "https://openweathermap.org/img/wn/" + data.list[15].weather[0].icon + ".png";
-      document.querySelector(".temp5").innerText = data.list[15].main.temp + "°C";
-      document.querySelector(".dt5").innerText = data.list[15].dt_txt;
-      
-      // 
     },
     search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
     },
   };
+
 
   document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
@@ -86,5 +104,11 @@ let weather = {
         weather.search();
       }
     });
-  
+    var ttt = "C";
+    document.querySelector("select").addEventListener("change", function (evt) {
+        ttt = evt.target.value;
+        console.log(ttt);
+        weather.fetchWeather(document.querySelector(".city").innerText.split("Weather in ")[1] || "Bangalore");
+    });
+
   weather.fetchWeather("Bangalore");
